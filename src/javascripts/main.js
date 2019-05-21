3955743bca03ceaeaaff583766795bbd7c6ff187
JPantana <joshpantana@me.com>
2019-05-20 19:54:59 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,11 @@
-import '../styles/main.scss';
+import firebase from 'firebase/app';
 import auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
+import apiKeys from './helpers/apiKeys.json';
+import '../styles/main.scss';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   auth.authStringBuilder();
   birfday.birfdayStringBuilder();
 };
