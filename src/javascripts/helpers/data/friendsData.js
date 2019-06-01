8e9f74fc12d8dfa4cc418dc3118a668802cf569f
JPantana <joshpantana@me.com>
2019-06-01 10:53:08 -0500

    detelion works

--- src/javascripts/helpers/data/friendsData.js
+++ src/javascripts/helpers/data/friendsData.js
@@ -4,8 +4,6 @@ import apiKeys from '../apiKeys.json';
 
 const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
 
-const addNewFriend = friendObject => axios.post(`${firebaseUrl}/friends.json`, friendObject);
-
 const getFriendsByUid = uid => new Promise((resolve, reject) => {
   axios.get(`${firebaseUrl}/friends.json?orderBy="uid"&equalTo="${uid}"`)
     .then((results) => {
@@ -20,4 +18,8 @@ const getFriendsByUid = uid => new Promise((resolve, reject) => {
     .catch(err => reject(err));
 });
 
-export default { addNewFriend, getFriendsByUid };
+const addNewFriend = friendObject => axios.post(`${firebaseUrl}/friends.json`, friendObject);
+
+const deleteFriend = friendId => axios.delete(`${firebaseUrl}/friends/${friendId}.json`);
+
+export default { addNewFriend, getFriendsByUid, deleteFriend };
